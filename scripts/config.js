28382d4cc2b69a417baaedc6bd1292f67eb732a5
moodymax <arnolda@gmail.com>
2022-07-19 04:36:50 -0700

    new comments check this out

--- scripts/config.js
+++ scripts/config.js
@@ -1,2 +1,4 @@
 const google = "https:/www.google.com"
 // hello fellow coder! if you're reading this it means I'm no longer working on that project. when I was working on that project only me and god knew what was written there. now only god knows,so I wish you a good luck! p.s ...
+
+// have a look at that
